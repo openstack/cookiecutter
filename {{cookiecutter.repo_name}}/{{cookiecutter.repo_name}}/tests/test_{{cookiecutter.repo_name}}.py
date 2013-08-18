a9380981145bac8ff349b8e3aa7d1c4fab8b8418
Monty Taylor <mordred@inaugust.com>
2013-08-18 15:07:10 -0400

    Update test code to be appropriate

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
@@ -1,5 +1,17 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
 """
 test_{{ cookiecutter.repo_name }}
@@ -8,21 +20,15 @@ test_{{ cookiecutter.repo_name }}
 Tests for `{{ cookiecutter.repo_name }}` module.
 """
 
-import unittest
+import testtools
 
-from {{ cookiecutter.repo_name }} import {{ cookiecutter.repo_name }}
+import {{ cookiecutter.repo_name }}
 
 
-class Test{{ cookiecutter.repo_name|capitalize }}(unittest.TestCase):
+class Test{{ cookiecutter.repo_name|capitalize }}(testtools.TestCase):
 
     def setUp(self):
-        pass
+        super(Test{{ cookiecutter.repo_name|capitalize }}, self).setUp()
 
     def test_something(self):
         pass
-
-    def tearDown(self):
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
