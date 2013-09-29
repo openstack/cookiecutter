ebe94d817fc752c155f5180a66abf22680eaf222
Monty Taylor <mordred@inaugust.com>
2013-09-29 15:43:32 -0400

    Move base test class out of __init__.py
    Change-Id: I7407556f2b276f9db20aeb5a243c5810250b41a4

--- {{cookiecutter.repo_name}}/{{cookiecutter.module_name}}/tests/test_{{cookiecutter.module_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.module_name}}/tests/test_{{cookiecutter.module_name}}.py
@@ -20,10 +20,10 @@ test_{{ cookiecutter.module_name }}
 Tests for `{{ cookiecutter.module_name }}` module.
 """
 
-from {{ cookiecutter.module_name }} import tests
+from {{ cookiecutter.module_name }}.tests import base
 
 
-class Test{{ cookiecutter.module_name|capitalize }}(tests.TestCase):
+class Test{{ cookiecutter.module_name|capitalize }}(base.TestCase):
 
     def test_something(self):
         pass
